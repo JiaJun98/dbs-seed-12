955293bae66d53ec7ed208ae41bb88019194c578
ernestteo97 <128830534+ernestteo97@users.noreply.github.com>
2023-05-06 13:37:00 +0800

    Log in API with Authentication

--- server/server.py
+++ server/server.py
@@ -19,37 +19,21 @@ def parse_json(data):
     return json.loads(json_util.dumps(data))
 CORS(app)
 jwt = JWTManager(app)
-#### Setting up MongoDB ####
+# #### Setting up MongoDB ####
 connection = 'mongodb://rwuser:Singapore123!@190.92.206.138:27017,159.138.120.227:27017/test?authSource=admin'
 client = MongoClient(connection)
 mydb = client.mydb
 
-##### Retrieve claim records list #####
-@app.route('/claim',methods=['GET'])
-@jwt_required()
-def get_transac():
-    claims = get_jwt()
-    userId = claims['id']
-    if mydb.user.find({"userId":userId}) and mydb.account.find({"userId":userId}):
-        if mydb.account.find({"userId":userId}):
-            account = mydb.account.find({"userId":userId})
-            account_data = parse_json(account)
-            account_id = account_data[0]["accountId"]
-            return ({"claims":parse_json(mydb.claim.find({"accountId":account_id}))}, 200)
-    else:
-        return {"message": "User is not authorised."}, 404
-    return {"message": "Account not found."}, 404
-
-#### Log In ####
+#User Log in 
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    user = mydb.user.find({"username":username})
+    employeeid = request.json.get("EmployeeID", None)
+    password = request.json.get("Password", None)
+    employee = mydb.Employee.find({"EmployeeID":employeeid})
     hash_password = bcrypt.generate_password_hash(password,10)
-    if user and bcrypt.check_password_hash(hash_password,parse_json(mydb.user.find({'password':password})[0]['password'])):
-        additional_claims = {"id": parse_json(user)[0]["userId"]}
-        access_token = create_access_token(identity=username,additional_claims=additional_claims)
+    if employee and bcrypt.check_password_hash(hash_password,parse_json(employee)[0]['Password']):
+        additional_claims = {"EmployeeID": parse_json(mydb.Employee.find({"EmployeeID":employeeid}))[0]['EmployeeID']}
+        access_token = create_access_token(identity=employeeid,additional_claims=additional_claims)
         return jsonify(access_token=access_token)
     return jsonify({"msg": "Bad username or password"}), 401
 
