0a32674b40ea7b7ec21a7a9144c2c6786673920c
l-w99 <ongliwen1304@gmail.com>
2023-05-06 11:39:26 +0800

    create form

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-import './App.css';
-=======
 import { Route, Routes } from "react-router-dom"
 // import { Home } from "./Home"
 // import { BookList } from "./BookList"
->>>>>>> ad1a5c25a7fc4204b721f79c11e149d8ca1edeb9
 
 export function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      
-    </div>
-  );
-}
-
-export default App;
-=======
     <Routes>
       {/* <Route path="/" element={<Home />} />
       <Route path="/books" element={<BookList />} /> */}
     </Routes>
   )
 }
-<<<<<<< HEAD
->>>>>>> ad1a5c25a7fc4204b721f79c11e149d8ca1edeb9
-=======
 export default App;
->>>>>>> a7365b264e8feeca922574eaf516a7aa69ff7ab2
--- client/src/components/createClaim.js
+++ client/src/components/createClaim.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const createClaim = () => {
   return (
     <div>
-      
+      <Form>
+      <Form.Group className="mb-3" controlId='formFirstName'>
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter First Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formLastName">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" placeholder="Last Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formProjectid">
+        <Form.Label>Project ID</Form.Label>
+        <Form.Select>
+          <option>select</option>
+        </Form.Select>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      </Form>
     </div>
   )
 }
