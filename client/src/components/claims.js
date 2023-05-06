5afbba34660d1239e9760034a13a9d749aa7408b
oleman159 <oleman_95@hotmail.com>
2023-05-06 12:39:02 +0800

    added mock data

--- client/src/components/claims.js
+++ client/src/components/claims.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 
+
+
 const ListClaims = () => {
     const [claims, setClaims] = useState([]);
 
     const getClaims = async () => {
         try {
-            const post = await fetch("http://localhost:5000/claims")
+            const post = await fetch('http://localhost:5000/claims')
         } catch (error) {
             console.error(error.message);
         }
@@ -14,6 +16,21 @@ const ListClaims = () => {
     useEffect(() => {
         getClaims();
     }, []);
+
+    const claim2 = [
+        {ClaimID: '11147',
+        ProjectID: '10001',
+        CurrencyID: 'SGD',
+        EmployeeID: '10011',
+        ExpenseDate: '2023-04-29T08:30:00+08:00',
+        Amount: '100.50',
+        Purpose: 'Banking tech',
+        ChargeToDefaultDept: '0',
+        AlternativeDeptCode: '',
+        Status: 'Pending',
+        LastEditedClaimDate: '2023-04-30T10:00:00+08:00'}
+      ]
+
 
     return(
         <>
@@ -28,30 +45,15 @@ const ListClaims = () => {
                 </tr>
             </thead>
             <tbody>
-                {claims.map(claim => (
+                {claim2.map(claim => (
                     <tr key={claim._id}>
                         <td>{claim.ClaimID}</td>
                         <td>{claim.ProjectID}</td>
                         <td>{claim.Amount} {claim.CurrencyID}</td>
                         <td>{claim.Status}</td>
                         <td>{claim.ClaimID}</td>
-                        <td>
-                            <div class="dropout">
-                                <button class="more">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                                </button>
-                                <ul>
-                                <li>
-                                    Edit
-                                </li>
-                                <li>
-                                    Delete
-                                </li>
-                                </ul>
-                            </div>
-                        </td>
+                        <td><button>Edit</button></td>
+                        <td><button>Delete</button></td>
                     </tr>
                 ))}
             </tbody>
